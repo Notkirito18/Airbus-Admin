7a32d7ee83a67e2842715f6a6f19ed51eb22d9e5
Notkirito18 <djallel123451@gmail.com>
2022-05-24 22:31:55 +0100

    slight fixes to home page

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { MediaChange, MediaObserver } from '@angular/flex-layout';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Subscription } from 'rxjs';
 
 @Component({
@@ -8,15 +9,26 @@ import { Subscription } from 'rxjs';
   styleUrls: ['./home-page.component.scss'],
 })
 export class HomePageComponent implements OnInit {
-  constructor(private mediaObserver: MediaObserver) {}
+  constructor(private mediaObserver: MediaObserver, private fb: FormBuilder) {}
   mediaSubscription!: Subscription;
   screenSize = 'lg';
+  contactForm!: FormGroup;
 
   ngOnInit(): void {
+    //* screen size traking
     this.mediaSubscription = this.mediaObserver.media$.subscribe(
       (result: MediaChange) => {
         this.screenSize = result.mqAlias;
       }
     );
+    //* contact form init
+    this.contactForm = this.fb.group({
+      email: [null, [Validators.required, Validators.email]],
+      subject: [null, Validators.required],
+      message: [null, Validators.required],
+    });
+  }
+  onFormSubmit() {
+    console.log(this.contactForm.value);
   }
 }
